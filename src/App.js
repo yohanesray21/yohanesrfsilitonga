bc3d61667fa297f12861c19e8fb0e18c2d2c699d
Yohanes Silitonga <yohanesrfsilitonga21@gmail.com>
2022-03-01 22:56:32 +0700

    Update HomePage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./components/HomePage";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+};
+
+export default App;
