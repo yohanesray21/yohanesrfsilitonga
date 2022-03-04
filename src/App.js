20647dfed4b89362170f8a6cf29b3321cb412bc5
Yohanes Silitonga <yohanesrfsilitonga21@gmail.com>
2022-03-04 23:57:53 +0700

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,107 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import {
+  AppBar,
+  Button,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import MenuIcon from "@mui/material/MenuItem";
+import { MaterialUISwitch } from "./buttons/MaterialUISwitch";
 
-import HomePage from "./components/HomePage";
+const pages = ["Products", "Pricing", "Blog"];
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* medium size */}
+            <Typography
+              variants="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            >
+              Yohanes
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <FormGroup>
+                <FormControlLabel
+                  control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+                  label=""
+                ></FormControlLabel>
+              </FormGroup>
+            </Box>
+            {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                // onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                // anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                // open={Boolean(anchorElNav)}
+                // onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box> */}
+
+            {/* Responsive Size */}
+            {/* <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            >
+              Yohanes
+            </Typography> */}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </React.Fragment>
   );
 };
 
